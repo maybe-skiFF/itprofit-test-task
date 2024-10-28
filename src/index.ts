b651fd45c266246df0dcef004276791878063dc5
maybe-skiFF <den.skiff190@gmail.com>
2024-10-29 00:00:33 +0300

    feat: add invalid input styles

--- src/index.ts
+++ src/index.ts
@@ -22,13 +22,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const errorsMessages = document.querySelectorAll('.error-message');
     errorsMessages.forEach((error) => error.remove());
 
+    const formInputs = feedBackForm.querySelectorAll('input');
+    formInputs.forEach((input) => input.classList.remove('invalid-input'));
+
+    const formTextArea = feedBackForm.querySelector('textarea');
+    formTextArea?.classList.remove('invalid-input');
+
     Object.keys(errors).forEach((error) => {
       const currFormInput = document.getElementById(error) as HTMLInputElement;
       if (currFormInput) {
-        const errorMessage = document.createElement('p');
-        errorMessage.classList.add('error-message');
-        errorMessage.textContent = errors[error as keyof IFormErrors] ?? '';
-        currFormInput.parentNode?.appendChild(errorMessage);
+        if (errors[error as keyof IFormErrors]) {
+          currFormInput.classList.add('invalid-input');
+          const errorMessage = document.createElement('p');
+          errorMessage.classList.add('error-message');
+          errorMessage.textContent = errors[error as keyof IFormErrors] ?? '';
+          currFormInput.parentNode?.appendChild(errorMessage);
+        }
       }
     });
 
